28b5deca219797d9c0f71ee3d690514edb68e73c
Brandon Lee <brandonlee781@gmail.com>
2017-11-28 23:00:43 -0600

    More updates to .graphql file use

--- src/routes/GraphQLRoute.ts
+++ src/routes/GraphQLRoute.ts
@@ -1,10 +1,11 @@
+import { resolvers } from '../schema/resolvers';
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 
 import { Environment, DB } from '../core';
+import { Schema } from '../core/Schema';
 import { Exception } from '../exceptions';
-import { schema } from '../schema';
+// import { schema } from '../schema';
 import {
   Context,
   ServicesContext
@@ -17,21 +18,28 @@ import {
   ProjectService,
   LogService
 } from '../services';
+import { Authenticate } from '../middleware/Authenticate';
+import { addResolveFunctionsToSchema } from 'graphql-tools';
 
 export class GraphQLRoute {
 
-  static map(app: express.Application): void {
+  static async map(app: express.Application): Promise<void> {
     GraphQLRoute.buildContext();
+    const schemaClass = new Schema();
+    const schema = await schemaClass.init();
 
-    app.use('/graphql', bodyParser.json(), graphqlExpress({ 
+    app.use('/graphql', Authenticate.authenticate(), graphqlExpress(request => ({ 
       schema,
       context: {
-        Services: ServicesContext.getInstance()
+        Services: ServicesContext.getInstance(),
+        user: request.user
       }
-    }));
+    })));
     if (Environment.getName() !== 'production') {
       app.use('/graphiql', graphiqlExpress({
-        endpointURL: '/graphql'
+        endpointURL: '/graphql',
+        // tslint:disable-next-line
+        passHeader: `'Authorization': 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImJyYW5kb25sZWU3ODFAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkcmYzMldyayJ9.o4D-YFd07Davg2jBQG1j80rYIzN0joCGfOpkNy79Vz0'`
       }));
     }
   }
