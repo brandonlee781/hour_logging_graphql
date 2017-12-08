205e508cc60067a80d47411e8d2dd97d7bbe8d30
Brandon Lee <brandonlee781@gmail.com>
2017-12-08 08:35:20 -0600

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'hour-logger-server',
+    script: 'dist/index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'branlee.me',
+      key: '~/.ssh/hour_logger.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:brandonlee781/hour_logging_graphql.git',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
